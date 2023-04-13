15adc7f3a2ed9396004d8127bfafb1d1a8ec0ae6
Morozzzuk <115870040+Morozzzuk@users.noreply.github.com>
2023-04-13 22:00:37 +0300

    add libraries

---
+++ src/index.js
@@ -0,0 +1,9 @@
+//^Add libraries and 
+//* Описаний в документації
+import SimpleLightbox from "simplelightbox";
+//* Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+import Notiflix from 'notiflix';
+import GalleryAPI from './js/GalleryAPI';
+// import LoadMoreBtn from './js/load-more-btn';
