1723e7430ef37f2848cf83b9115cd5d2714286fb
Morozzzuk <115870040+Morozzzuk@users.noreply.github.com>
2023-04-14 17:45:07 +0300

    update

--- src/js/GalleryAPI.js
+++ src/js/GalleryAPI.js
@@ -4,26 +4,26 @@ import axios from 'axios';
 //^Add consts 
 const API_KEY = '35381512-dde9d5be5a6a0ea2b84aeda4e';
 const BASE_URL = 'https://pixabay.com/api/';
-const PER_PAGE = 60;
+const PER_PAGE = 40;
 
 export default class GalleryAPI {
-    constructor() {
-        this.searchQuery = '';
-        this.page = 1;
-        this.perPage = PER_PAGE;
-    }
-     async getPopularPhotos() {
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
+    this.perPage = PER_PAGE;
+  }
+  async getPopularPhotos() {
     const response = await axios.get(
       `${BASE_URL}?key=${API_KEY}&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${this.page}`
     );
     const data = await response.data;
     this.page += 1;
     return data;
-     }
-    resetPage() {
+  }
+  resetPage() {
     this.page = 1;
-    }
-    set query(newQuery) {
+  }
+  set query(newQuery) {
     this.searchQuery = newQuery;
-    }
+  }
 }
