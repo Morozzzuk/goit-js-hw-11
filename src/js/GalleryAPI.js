313f8aa71ad6803384da31a3f79c54a587fe37ba
Morozzzuk <115870040+Morozzzuk@users.noreply.github.com>
2023-04-24 20:47:24 +0300

    update

--- src/js/GalleryAPI.js
+++ src/js/GalleryAPI.js
@@ -26,6 +26,11 @@ export default class GalleryAPI {
   resetPage() {
     this.page = 1;
   }
+
+  get query() {
+    return this.searchQuery;
+  }
+  
   set query(newQuery) {
     this.searchQuery = newQuery;
   }
